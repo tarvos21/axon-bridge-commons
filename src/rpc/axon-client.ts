2edb130789b8823b09405823140d62c575944e5d
ashuralyk <ashuralyk@live.com>
2022-07-26 17:57:01 +0800

    feat: complete Web3 module in crosschain path from Axon to CKB

--- src/rpc/axon-client.ts
+++ src/rpc/axon-client.ts
@@ -1,21 +1,39 @@
-import { ethers } from 'ethers';
-import { JSONRPCClient, JSONRPCRequest } from 'json-rpc-2.0';
-import fetch from 'node-fetch';
-import CrossChain from '../../src/abi/CrossChain.json';
-import ERC20 from '../../src/abi/ERC20.json';
-import { API, AssetType, NetworkBase, NetworkTypes, RequiredAsset } from '../types';
-import { GetBalanceResponse } from '../types/apiv1';
-import { stringToUint8Array } from '../utils/index';
+import { ethers } from "ethers";
+import { JSONRPCClient, JSONRPCRequest } from "json-rpc-2.0";
+import fetch from "node-fetch";
+import CrossChain from "../../src/abi/CrossChain.json";
+import ERC20 from "../../src/abi/ERC20.json";
+import {
+  API,
+  AssetType,
+  NetworkBase,
+  NetworkTypes,
+  RequiredAsset,
+} from "../types";
+import { GetBalanceResponse } from "../types/apiv1";
+import { stringToUint8Array } from "../utils/index";
 
 export class AxonApiHandler implements API.ForceBridgeAPIV1 {
   provider: ethers.providers.JsonRpcProvider;
   crossChainAddress: string;
   crossChainContract: ethers.Contract;
+  wCkbAddress: string;
+  wCkbContract: ethers.Contract;
 
   constructor(axonRpcUrl: string) {
     this.provider = new ethers.providers.JsonRpcProvider(axonRpcUrl);
-    this.crossChainAddress = '0xf67bc4e50d1df92b0e4c61794a4517af6a995cb2';
-    this.crossChainContract = new ethers.Contract(this.crossChainAddress, CrossChain.abi, this.provider);
+    this.crossChainAddress = "0xf67bc4e50d1df92b0e4c61794a4517af6a995cb2";
+    this.crossChainContract = new ethers.Contract(
+      this.crossChainAddress,
+      CrossChain.abi,
+      this.provider
+    );
+    this.wCkbAddress = "0x4af5ec5e3d29d9ddd7f4bf91a022131c41b72352";
+    this.wCkbContract = new ethers.Contract(
+      this.wCkbAddress,
+      ERC20.abi,
+      this.provider
+    );
     // eslint-disable-next-line no-console
     console.log(`crossChainContract: ${this.crossChainContract}`);
   }
@@ -41,65 +59,124 @@ export class AxonApiHandler implements API.ForceBridgeAPIV1 {
   // );
   // }
 
-  getMinimalBridgeAmount(payload: API.GetMinimalBridgeAmountPayload): Promise<API.GetMinimalBridgeAmountResponse> {
+  getMinimalBridgeAmount(
+    payload: API.GetMinimalBridgeAmountPayload
+  ): Promise<API.GetMinimalBridgeAmountResponse> {
     // return Promise.resolve(this.client.request('getMinimalBridgeAmount', payload));
     return (async () => {
-      const TokenContract = new ethers.Contract(this.crossChainAddress, CrossChain.abi, this.provider);
       // const erc20_amount = await TokenContract.getWCKBMin();
-      const erc20_amount = await TokenContract.fee(payload.xchainAssetIdent, payload.amount);
+      const erc20_amount = await this.crossChainContract.fee(
+        payload.xchainAssetIdent,
+        payload.amount
+      );
       const minimalAmount = erc20_amount.toString();
       return { minimalAmount };
     })();
   }
 
+  getMinimalBridgeWCKBAmount(): Promise<API.GetMinimalBridgeAmountResponse> {
+    // return Promise.resolve(this.client.request('getMinimalBridgeAmount', payload));
+    return (async () => {
+      // const erc20_amount = await TokenContract.getWCKBMin();
+      const min_wckb_amount = await this.crossChainContract.getWCKBMin();
+      const minimalAmount = min_wckb_amount.toString();
+      return { minimalAmount };
+    })();
+  }
+
+  getTokenAddressWhiteList(): Promise<API.getTokenAddressWhiteListResponse> {
+    return (async () => {
+      const tokenAddressWhitelist = await this.crossChainContract.whitelist();
+      return { tokenAddressWhitelist };
+    })();
+  }
+
   getBridgeInNervosBridgeFee(
-    payload: API.GetBridgeInNervosBridgeFeePayload,
+    payload: API.GetBridgeInNervosBridgeFeePayload
   ): Promise<API.GetBridgeInNervosBridgeFeeResponse> {
     // return Promise.resolve(this.client.request('getBridgeInNervosBridgeFee', payload));
     // eslint-disable-next-line no-console
     console.log(`getBridgeInNervosBridgeFee: ${JSON.stringify(payload)}`);
     return (async () => {
-      const fee_amount = await this.crossChainContract.fee(payload.xchainAssetIdent, payload.amount);
+      const fee_amount = await this.crossChainContract.fee(
+        payload.xchainAssetIdent,
+        payload.amount
+      );
       const fee = fee_amount.toString();
-      console.log(`getBridgeInNervosBridgeFee: ${JSON.stringify(payload)}, ${fee}`);
+      console.log(
+        `getBridgeInNervosBridgeFee: ${JSON.stringify(payload)}, ${fee}`
+      );
       return { fee };
     })();
   }
 
   getBridgeOutNervosBridgeFee(
-    payload: API.GetBridgeOutNervosBridgeFeePayload,
+    payload: API.GetBridgeOutNervosBridgeFeePayload
   ): Promise<API.GetBridgeOutNervosBridgeFeeResponse> {
     // return Promise.resolve(Promise.resolve(this.client.request('getBridgeOutNervosBridgeFee', payload)));
-    return Promise.reject(new Error('not implementation'));
+    return Promise.reject(new Error("not implementation"));
   }
 
   async generateBridgeInNervosTransaction<T extends NetworkTypes>(
-    payload: API.GenerateBridgeInTransactionPayload,
+    payload: API.GenerateBridgeInTransactionPayload
   ): Promise<API.GenerateTransactionResponse<T>> {
     // eslint-disable-next-line no-console
-    console.log(`generateBridgeInNervosTransaction, payload: ${JSON.stringify(payload)}`);
+    console.log(
+      `generateBridgeInNervosTransaction, payload: ${JSON.stringify(payload)}`
+    );
+
+    // check allowance of wCKB
+    let allowance = await this.wCkbContract.allowance(
+      payload.sender,
+      this.crossChainAddress
+    );
+    if (allowance != ethers.constants.MaxUint256) {
+      let result = await this.wCkbContract.approve(
+        this.crossChainAddress,
+        ethers.constants.MaxUint256
+      );
+      console.log("set allowance of wCKB:", result);
+    }
 
     let tx;
     switch (payload.asset.network) {
-      case 'Ethereum': {
-        const sudtExtraData = '0x';
-        const ethAmount = ethers.utils.parseUnits(payload.asset.amount, 0);
+      case "Ethereum": {
+        const transferAmount = ethers.utils.parseUnits(payload.asset.amount, 0);
         const recipient = stringToUint8Array(payload.recipient);
 
         switch (payload.asset.ident) {
-          case '0x0000000000000000000000000000000000000000':
-            tx = await this.crossChainContract.populateTransaction.lockAT(payload?.recipient, {
-              value: ethAmount,
-            });
+          case "0x0000000000000000000000000000000000000000":
+            tx = await this.crossChainContract.populateTransaction.lockAT(
+              payload?.recipient,
+              {
+                value: transferAmount,
+              }
+            );
             break;
           default:
-            // TODO lock other tokens
-            tx = await this.crossChainContract.populateTransaction.lockToken(
+            // check allowance of crossing erc20 token
+            let tokenContract = new ethers.Contract(
               payload.asset.ident,
-              ethAmount,
-              recipient,
-              sudtExtraData,
+              ERC20.abi,
+              this.provider
             );
+            allowance = await tokenContract.allowance(
+              payload.sender,
+              this.crossChainAddress
+            );
+            if (allowance != ethers.constants.MaxUint256) {
+              let result = await tokenContract.approve(
+                this.crossChainAddress,
+                ethers.constants.MaxUint256
+              );
+              console.log("set allowance of cross token:", result);
+            }
+            tx =
+              await this.crossChainContract.populateTransaction.crossTokenToCKB(
+                recipient,
+                payload.asset.ident,
+                transferAmount
+              );
             break;
         }
         tx.value = ethers.BigNumber.from(tx.value?._hex ?? 0);
@@ -107,57 +184,60 @@ export class AxonApiHandler implements API.ForceBridgeAPIV1 {
           network: payload.asset.ident,
           rawTransaction: tx,
         };
-        break;
       }
       default:
-        throw new Error('invalid chain type');
+        throw new Error("invalid chain type");
     }
   }
 
   async generateBridgeOutNervosTransaction<T extends NetworkTypes>(
-    payload: API.GenerateBridgeOutNervosTransactionPayload,
+    payload: API.GenerateBridgeOutNervosTransactionPayload
   ): Promise<API.GenerateTransactionResponse<T>> {
     // return this.client.request('generateBridgeOutNervosTransaction', payload);
-    return Promise.reject(new Error('not implementation'));
+    return Promise.reject(new Error("not implementation"));
   }
 
   async sendSignedTransaction<T extends NetworkBase>(
-    payload: API.SignedTransactionPayload<T>,
+    payload: API.SignedTransactionPayload<T>
   ): Promise<API.TransactionIdent> {
     // return this.client.request('sendSignedTransaction', payload);
-    return Promise.reject(new Error('not implementation'));
+    return Promise.reject(new Error("not implementation"));
   }
 
   async getBridgeTransactionStatus(
-    payload: API.GetBridgeTransactionStatusPayload,
+    payload: API.GetBridgeTransactionStatusPayload
   ): Promise<API.GetBridgeTransactionStatusResponse> {
     // return this.client.request('getBridgeTransactionStatus', payload);
-    return Promise.reject(new Error('not implementation'));
+    return Promise.reject(new Error("not implementation"));
   }
 
   async getBridgeTransactionSummaries(
-    payload: API.GetBridgeTransactionSummariesPayload,
+    payload: API.GetBridgeTransactionSummariesPayload
   ): Promise<API.TransactionSummaryWithStatus[]> {
     // return await this.client.request('getBridgeTransactionSummaries', payload);
     return Promise.resolve([]);
   }
 
-  async getAssetList(name?: string): Promise<RequiredAsset<'info'>[]> {
+  async getAssetList(name?: string): Promise<RequiredAsset<"info">[]> {
     let param = { asset: name };
     if (name == undefined) {
-      param = { asset: 'all' };
+      param = { asset: "all" };
     }
     // return this.client.request('getAssetList', param);
     return Promise.resolve([
       {
-        network: 'Ethereum',
-        ident: '0x0000000000000000000000000000000000000000',
+        network: "Ethereum",
+        ident: "0x0000000000000000000000000000000000000000",
         info: {
           decimals: 18,
-          name: 'AT',
-          symbol: 'AT',
-          logoURI: 'https://cryptologos.cc/logos/ethereum-eth-logo.svg?v=002',
-          shadow: { network: 'Nervos', ident: '0xb3fdbe0ddaf0b8d3e427fd56968a1eaefa7b57acb36cce34bafebeae22e0ebef' },
+          name: "AT",
+          symbol: "AT",
+          logoURI: "https://cryptologos.cc/logos/ethereum-eth-logo.svg?v=002",
+          shadow: {
+            network: "Nervos",
+            ident:
+              "0xb3fdbe0ddaf0b8d3e427fd56968a1eaefa7b57acb36cce34bafebeae22e0ebef",
+          },
         },
       },
       // {
@@ -174,25 +254,31 @@ export class AxonApiHandler implements API.ForceBridgeAPIV1 {
     ]);
   }
 
-  async getBalance(payload: API.GetBalancePayload): Promise<API.GetBalanceResponse> {
+  async getBalance(
+    payload: API.GetBalancePayload
+  ): Promise<API.GetBalanceResponse> {
     // return this.client.request('getBalance', payload);
     const balanceFutures: Promise<AssetType>[] = payload.map(async (p) => {
       let balance: string;
       const tokenAddress = p.assetIdent;
       const userAddress = p.userIdent;
       switch (p.network) {
-        case 'Axon':
-        case 'Ethereum':
-          if (tokenAddress === '0x0000000000000000000000000000000000000000') {
+        case "Axon":
+        case "Ethereum":
+          if (tokenAddress === "0x0000000000000000000000000000000000000000") {
             const eth_amount = await this.provider.getBalance(userAddress);
             balance = eth_amount.toString();
           } else {
-            const TokenContract = new ethers.Contract(tokenAddress, ERC20.abi, this.provider);
+            const TokenContract = new ethers.Contract(
+              tokenAddress,
+              ERC20.abi,
+              this.provider
+            );
             const erc20_amount = await TokenContract.balanceOf(userAddress);
             balance = erc20_amount.toString();
           }
           break;
-        case 'Nervos': {
+        case "Nervos": {
           /*
           const userScript = parseAddress(value.userIdent);
           const sudtType = {
@@ -205,14 +291,14 @@ export class AxonApiHandler implements API.ForceBridgeAPIV1 {
           balance = sudt_amount.toString();
           break;
            */
-          balance = '0';
+          balance = "0";
           break;
         }
         default:
-          throw new Error('invalid chain type');
+          throw new Error("invalid chain type");
       }
       return {
-        network: p.network as 'Nervos' | 'Axon',
+        network: p.network as "Nervos" | "Axon",
         ident: p.assetIdent,
         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
         amount: balance!,
@@ -225,19 +311,27 @@ export class AxonApiHandler implements API.ForceBridgeAPIV1 {
     //     amount: '4525094745388951945',
     //   },
     // ]);
-    return (await Promise.all(balanceFutures)) as unknown as Promise<GetBalanceResponse>;
+    return (await Promise.all(
+      balanceFutures
+    )) as unknown as Promise<GetBalanceResponse>;
   }
 
   async getBridgeConfig(): Promise<API.GetConfigResponse> {
     // return this.client.request('getBridgeConfig');
     return Promise.resolve({
       nervos: {
-        network: 'testnet',
+        network: "testnet",
         confirmNumber: 15,
-        omniLockCodeHash: '0xea5d73e46455979b498e7c6eb4eb88af285ad474c3a7eab98d16e0d9210d56f1',
-        omniLockHashType: 'data',
+        omniLockCodeHash:
+          "0xea5d73e46455979b498e7c6eb4eb88af285ad474c3a7eab98d16e0d9210d56f1",
+        omniLockHashType: "data",
       },
-      xchains: { Ethereum: { contractAddress: '0xf67bc4e50d1df92b0e4c61794a4517af6a995cb2', confirmNumber: 12 } },
+      xchains: {
+        Ethereum: {
+          contractAddress: "0xf67bc4e50d1df92b0e4c61794a4517af6a995cb2",
+          confirmNumber: 12,
+        },
+      },
     });
   }
 }
--- src/types/apiv1.ts
+++ src/types/apiv1.ts
@@ -1,10 +1,15 @@
-import { HashType } from '@ckb-lumos/lumos';
-import { NervosNetwork, NetworkBase, NetworkTypes, RequiredAsset } from './network';
+import { HashType } from "@ckb-lumos/lumos";
+import {
+  NervosNetwork,
+  NetworkBase,
+  NetworkTypes,
+  RequiredAsset,
+} from "./network";
 
 export enum BridgeTransactionStatus {
-  Pending = 'Pending',
-  Successful = 'Successful',
-  Failed = 'Failed',
+  Pending = "Pending",
+  Successful = "Successful",
+  Failed = "Failed",
 }
 
 /* unix timestamp in milliseconds */
@@ -12,23 +17,30 @@ type Timestamp = number;
 export type TransactionIdent = { txId: string };
 export type TransactionSummary = {
   txSummary: {
-    fromAsset: RequiredAsset<'amount'>;
-    toAsset: RequiredAsset<'amount'>;
-    fromTransaction: TransactionIdent & { timestamp: Timestamp } & { confirmStatus: 'pending' | number | 'confirmed' };
+    fromAsset: RequiredAsset<"amount">;
+    toAsset: RequiredAsset<"amount">;
+    fromTransaction: TransactionIdent & { timestamp: Timestamp } & {
+      confirmStatus: "pending" | number | "confirmed";
+    };
     toTransaction?: TransactionIdent & { timestamp?: Timestamp };
   };
 };
-export type FailedTransactionSummary = TransactionSummary & { status: BridgeTransactionStatus.Failed; message: string };
+export type FailedTransactionSummary = TransactionSummary & {
+  status: BridgeTransactionStatus.Failed;
+  message: string;
+};
 export type UnFailedTransactionSummary = TransactionSummary & {
   status: BridgeTransactionStatus.Pending | BridgeTransactionStatus.Successful;
 };
 
-export type TransactionSummaryWithStatus = UnFailedTransactionSummary | FailedTransactionSummary;
+export type TransactionSummaryWithStatus =
+  | UnFailedTransactionSummary
+  | FailedTransactionSummary;
 
 // XChain -> Nervos
 export type GenerateBridgeInTransactionPayload = {
-  asset: RequiredAsset<'amount'>;
-  recipient: NervosNetwork['UserIdent'];
+  asset: RequiredAsset<"amount">;
+  recipient: NervosNetwork["UserIdent"];
   // XChain user ident
   sender: string;
 };
@@ -43,19 +55,19 @@ export type GenerateBridgeOutNervosTransactionPayload = {
   amount: string;
   // XChain User ident
   recipient: string;
-  sender: NervosNetwork['UserIdent'];
+  sender: NervosNetwork["UserIdent"];
 };
 
 export type GenerateTransactionResponse<N extends NetworkTypes> = {
   network: string;
   // TODO
-  rawTransaction: N['RawTransaction'];
+  rawTransaction: N["RawTransaction"];
 };
 
 export type SignedTransactionPayload<N extends NetworkBase> = {
-  network: N['Network'];
+  network: N["Network"];
   // TODO
-  signedTransaction: N['SignedTransaction'];
+  signedTransaction: N["SignedTransaction"];
 };
 
 export type GetBalancePayload = Array<{
@@ -64,7 +76,7 @@ export type GetBalancePayload = Array<{
   assetIdent: string;
 }>;
 
-export type GetBalanceResponse = Array<RequiredAsset<'amount'>>;
+export type GetBalanceResponse = Array<RequiredAsset<"amount">>;
 
 export type GetBridgeTransactionStatusPayload = {
   network: string;
@@ -95,6 +107,10 @@ export interface GetMinimalBridgeAmountResponse {
   minimalAmount: string;
 }
 
+export interface getTokenAddressWhiteListResponse {
+  tokenAddressWhitelist: string[];
+}
+
 export interface GetBridgeInNervosBridgeFeePayload {
   network: string;
   xchainAssetIdent: string;
@@ -102,7 +118,7 @@ export interface GetBridgeInNervosBridgeFeePayload {
 }
 
 export interface GetBridgeInNervosBridgeFeeResponse {
-  fee: RequiredAsset<'amount'>;
+  fee: RequiredAsset<"amount">;
 }
 
 export interface GetBridgeOutNervosBridgeFeePayload {
@@ -112,7 +128,7 @@ export interface GetBridgeOutNervosBridgeFeePayload {
 }
 
 export interface GetBridgeOutNervosBridgeFeeResponse {
-  fee: RequiredAsset<'amount'>;
+  fee: RequiredAsset<"amount">;
 }
 
 export interface EthereumConfig {
@@ -122,7 +138,7 @@ export interface EthereumConfig {
 
 export interface GetConfigResponse {
   nervos: {
-    network: 'mainnet' | 'testnet';
+    network: "mainnet" | "testnet";
     confirmNumber: number;
     omniLockCodeHash: string;
     omniLockHashType: HashType;
@@ -142,7 +158,9 @@ export interface ForceBridgeAPIV1 {
   generateBridgeOutNervosTransaction: <T extends NetworkTypes>(payload: GenerateBridgeOutNervosTransactionPayload) => Promise<GenerateTransactionResponse<T>>;
 
   /* send transaction */
-  sendSignedTransaction: <T extends NetworkBase>(payload: SignedTransactionPayload<T>) => Promise<TransactionIdent>;
+  sendSignedTransaction: <T extends NetworkBase>(
+    payload: SignedTransactionPayload<T>
+  ) => Promise<TransactionIdent>;
 
   /* get transaction summary */
   // prettier-ignore
@@ -154,7 +172,7 @@ export interface ForceBridgeAPIV1 {
   getBridgeTransactionSummaries: (payload: GetBridgeTransactionSummariesPayload) => Promise<TransactionSummaryWithStatus[]>;
 
   // get an asset list, or if no `name` param is passed in, return a default list of whitelisted assets
-  getAssetList: (name?: string) => Promise<RequiredAsset<'info'>[]>;
+  getAssetList: (name?: string) => Promise<RequiredAsset<"info">[]>;
   // get the user's balance, or if no `assets` param is passed in, return all whitelisted assets
   // prettier-ignore
   getBalance: (payload: GetBalancePayload) => Promise<GetBalanceResponse>;
